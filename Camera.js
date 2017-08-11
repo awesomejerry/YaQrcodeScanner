4cfa5fe4804a0e7e6066b7126512353b7087369a
JerryShen <jerry82422@gmail.com>
2017-08-11 09:09:19 +0800

    Add `openURL` to `Camera`

--- Camera.js
+++ Camera.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
-import Camera from 'react-native-camera';
-import Toast from 'react-native-root-toast';
-import styles from './styles';
+import React, { Component } from "react";
+import { Linking } from "react-native";
+import Camera from "react-native-camera";
+import Toast from "react-native-root-toast";
+import styles from "./styles";
+
+const TOAST_OPTION = { position: Toast.positions.CENTER };
 
 export default class MyCamera extends Component {
-  handleBarCodeRead = (e) => {
-    Toast.show(e.data, {
-      position: Toast.positions.CENTER
-    });
+  handleBarCodeRead = e => {
+    const url = e.data;
+
+    Linking.canOpenURL(url)
+      .then(supported => {
+        if (!supported) {
+          Toast.show(url, TOAST_OPTION);
+        } else {
+          return Linking.openURL(url);
+        }
+      })
+      .catch(err => Toast.show("An error occurred...", TOAST_OPTION));
   };
   render() {
     return (
       <Camera
-        ref={(c) => (this.camera = c)}
+        ref={c => (this.camera = c)}
         style={styles.camera}
         aspect={Camera.constants.Aspect.fill}
         onBarCodeRead={this.handleBarCodeRead}
